68238edc7ecbbfd09abc31b22561d12652f1c073
Letícia dos Santos <lds.leticia.dos.santos@gmail.com>
2022-02-14 19:46:10 +0000

    Ajustes na lógica e trava de quebra

--- jogo_de_dados/dados.py
+++ jogo_de_dados/dados.py
@@ -3,10 +3,19 @@ import random
 while True:
     print()
     pergunta = input(
-        "Voce quer continuar rolando os dados? Caso não queira, digite não "
+        "Voce quer continuar rolando os dados? Digite sim para continuar ou não se quiser parar o jogo "
     )
     print()
-    valor_aleatorio = random.randint(1, 6)
-    print(f"valor gerado foi {valor_aleatorio}")
+    if pergunta != "sim" and pergunta != "nao":
+        print("Não entendi o que voce disse, por favor insira uma entrada correta")
+    elif (
+        pergunta == "não" or pergunta == "Não" or pergunta == "nao" or pergunta == "Nao"
+    ):
+        print("Obrigada por jogar com nossos dados")
+        break
+    else:
+        valor_aleatorio = random.randint(1, 6)
+        print(f"valor gerado foi {valor_aleatorio}")
     if pergunta == "não" or pergunta == "Não" or pergunta == "nao" or pergunta == "Nao":
+        print("Obrigada por jogar com nossos dados")
         break
