aa0e5bfbc10e56ccfbbbeebc2fc525b05b4a61f5
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-02-15 01:30:02 -0300

    decida_por_mim

---
+++ decida_por_mim/decida_por_mim.py
@@ -0,0 +1,25 @@
+frases = (
+    "Só vai",
+    "Não acho boa ideia",
+    "Talvez dê certo",
+    "Talvez dê errado",
+    "Não julgo",
+    "Não julgo, faria o mesmo",
+    "Não julgo mas não faria o mesmo",
+    "Ouvi dizer que isso não é bom",
+    "Ouvi dizer que isso é bom",
+    "Gosto de você, mas as vezes você me irrita",
+    "Você realmente acha que daria certo?",
+    "É meu jeito ninja de ser",
+    "O fracasso não é razão para você desistir, desde que continue acreditando",
+    "As coisas mais importantes não estão escritas num livro, é preciso aprendê-las vivenciando-as sozinho",
+)
+
+escolha = ""
+
+while escolha != "sair":
+    escolha = int(
+        input(
+            "Me pergunte o que quiser e te darei uma resposta. Se quiser sair do nosso bate papo, digite sair "
+        )
+    )
